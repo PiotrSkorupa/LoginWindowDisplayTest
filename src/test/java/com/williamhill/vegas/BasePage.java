cbec5fd3c1cb7a1e00cb69b7db1068eaf5a58f6f
Piotr Skorupa <32395394+PiotrSkorupa@users.noreply.github.com>
2018-10-11 09:30:12 +0200

    web elements added

---
+++ src/test/java/com/williamhill/vegas/BasePage.java
@@ -0,0 +1,7 @@
+package com.williamhill.vegas;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+    protected WebDriver driver;
+}
--- src/test/java/com/williamhill/vegas/VegasPage.java
+++ src/test/java/com/williamhill/vegas/VegasPage.java
@@ -1,5 +1,33 @@
 package com.williamhill.vegas;
 
-public class VegasPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class VegasPage extends BasePage {
+
+    private WebDriverWait wait;
+
+    public VegasPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(className = "btn-search-magnifier")
+    private WebElement searchButton;
+
+    @FindBy(css = ".cHTTOm")
+    private WebElement searchBox;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div[3]/section/div/div/div[1]/div/div/div/div[3]/div/button[2]")
+    private WebElement moreButton;
+
+    @FindBy(xpath = "//div[contains(text(), 'Play now')]")
+    private WebElement playButton;
+
+    @FindBy(xpath = "/html/body/div[3]/div/div/div[2]/div[3]/div/div/div[3]/section/div/div/div[1]/div/div/div/div[1]/div/img")
+    private WebElement hoverOverTo;
 
 }
