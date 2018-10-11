378e45350778f1dac21638e421fe4ec00215a2be
Piotr Skorupa <32395394+PiotrSkorupa@users.noreply.github.com>
2018-10-11 10:15:22 +0200

    added test shouldDisplayLoginWindow

--- src/test/java/com/williamhill/vegas/DisplayLoginWindowTest.java
+++ src/test/java/com/williamhill/vegas/DisplayLoginWindowTest.java
@@ -3,21 +3,41 @@ package com.williamhill.vegas;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static junit.framework.TestCase.assertTrue;
 
 public class DisplayLoginWindowTest {
 
+    private String gameName = "Mayfair Roulette";
+    private final String BASE_URL = "https://vegas.williamhill.com/";
+    private WebDriver driver;
+    private VegasPage vegasPage;
+
     @Before
     public void setUp() {
-
+        System.setProperty("webdriver.chrome.driver", "src/test/java/com/williamhill/vegas/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(BASE_URL);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
     }
 
     @Test
     public void shouldDisplayLoginWindow() {
-
+        vegasPage = new VegasPage(driver);
+        vegasPage.searchFor(gameName)
+                .clickMoreButton(driver)
+                .playGame();
+        assertTrue(driver.findElement(By.className("login-component__wrapper")).isDisplayed());
     }
 
     @After
     public void tearDown() {
-
+        driver.quit();
     }
 }
--- src/test/java/com/williamhill/vegas/VegasPage.java
+++ src/test/java/com/williamhill/vegas/VegasPage.java
@@ -2,18 +2,15 @@ package com.williamhill.vegas;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class VegasPage extends BasePage {
 
     private WebDriverWait wait;
-
-    public VegasPage(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
-    }
 
     @FindBy(className = "btn-search-magnifier")
     private WebElement searchButton;
@@ -30,4 +27,29 @@ public class VegasPage extends BasePage {
     @FindBy(xpath = "/html/body/div[3]/div/div/div[2]/div[3]/div/div/div[3]/section/div/div/div[1]/div/div/div/div[1]/div/img")
     private WebElement hoverOverTo;
 
+    public VegasPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public VegasPage searchFor(String text) {
+        searchButton.click();
+        searchBox.sendKeys(text);
+        return this;
+    }
+
+    public VegasPage clickMoreButton(WebDriver driver) {
+        new Actions(driver)
+                .moveToElement(hoverOverTo)
+                .perform();
+        wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOf(moreButton));
+        moreButton.click();
+        return this;
+    }
+
+    public VegasPage playGame() {
+        playButton.click();
+        return this;
+    }
 }
